fab0339d70747b90e81ce789b18c12a7e0f4a6f5
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2019-03-18 11:29:02 -0600

    Lesson: Argument Parsing with Yargs: Part I

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -1,19 +1,47 @@
-// const getNotes = require('./notes.js')
-
-// const msg = getNotes()
-// console.log(msg)
-
-/**
- * Challenge: Use the chalk library in your project
- * 
- * 1. Install version 2.4.1 of chalk
- * 2. Load chalk into app.js
- * 3. Use it to print the string "Success!" to the console in green 
- * 4. Test your work
- * 
- * Bonus: Use docs to mess around with other styles. Make text bold and inversed.
- */
-
 const chalk = require('chalk')
-const greenMsg = chalk.green.inverse.bold('Success!')
-console.log(greenMsg);
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
+
+// Customize yargs version
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function() {
+    console.log('Adding a new note!')
+  }
+})
+
+// Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove the note',
+  handler: function() {
+    console.log('Removing the note')
+  }
+})
+
+// Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List your notes',
+  handler: function() {
+    console.log('Listing out all notes')
+  }
+})
+
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: function() {
+    console.log('Reading a note')
+  }
+})
+
+// add, remove, read, list
+
+
+console.log(process.argv)
+console.log(yargs.argv)
