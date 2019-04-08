971f365d9603bec5de02bb2373057ae40955fe54
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2019-04-07 21:45:27 -0500

    Lesson: Removing a Note

--- node-course/notes-app/app.js
+++ node-course/notes-app/app.js
@@ -30,8 +30,15 @@ yargs.command({
 yargs.command({
   command: 'remove',
   describe: 'Remove the note',
-  handler: function() {
-    console.log('Removing the note')
+  builder: {
+    title: {
+      describe: 'Note to remove',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: function(argv) {
+    notes.removeNote(argv.title)
   }
 })
 
--- node-course/notes-app/notes.js
+++ node-course/notes-app/notes.js
@@ -22,6 +22,20 @@ const addNote = function(title, body) {
   }
 }
 
+const removeNote = function(title) {
+  const notes = loadNotes()
+  const filteredNotes = notes.filter(function (note) {
+    return note.title !== title
+  })
+  
+  if(notes.length === filteredNotes.length) {
+    console.log('Note not found!')
+  } else {
+    saveNotes(filteredNotes)
+    console.log(title + ' removed.')
+  }
+}
+
 const saveNotes = function (notes) {
   const dataJSON = JSON.stringify(notes)
   fs.writeFileSync('notes.json', dataJSON)
@@ -39,5 +53,6 @@ const loadNotes = function () {
 
 module.exports = {
   getNotes: getNotes,
-  addNote: addNote
+  addNote: addNote,
+  removeNote: removeNote
 }
